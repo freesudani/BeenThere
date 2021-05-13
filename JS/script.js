4d38726e4ae265dc17a255b025b2ee4f8dc03a5a
freesudani <mohamed.dahiya@hotmail.com>
2021-05-13 15:09:59 +0700

    adding google map

--- JS/script.js
+++ JS/script.js
@@ -15,7 +15,28 @@ if (navigator.geolocation)
 navigator.geolocation.getCurrentPosition(function(position) {
     const {latitude} = position.coords;
     const {longitude} = position.coords;
-    console.log(latitude,longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude,longitude]
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    }).addTo(map);
+    
+    
+    map.on('click', function(mapEvent) {
+        console.log(mapEvent)
+
+        const {lat , lng } = mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(map)
+        .bindPopup('Visited')
+        .openPopup();     
+    
+    })    
 
 }, function() {
     alert('Could not get you location')
